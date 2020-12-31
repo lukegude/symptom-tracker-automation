cef6a0994cee0ce622fcc4257ccf65ea35120986
lukegude <lukegude@gmail.com>
2020-12-30 20:38:36 -0500

    Add files via upload

---
+++ configurator.py
@@ -0,0 +1,17 @@
+"""
+Start here to set your config file
+"""
+import pickle
+from main import User
+
+
+def new_user(username, password, phone):
+    with open('config.json', 'wb') as f:
+        pickle.dump(User(username, password, phone), f)
+if __name__ == "__main__":
+    choice = input('New user? (Y/n) ')
+    if choice.lower() == 'y':
+        username = input('UMBC Username: ')
+        password = input('UMBC Password: ')
+        phone = input('Phone Number: ')
+        new_user(username, password, phone)
